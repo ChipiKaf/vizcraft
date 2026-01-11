f9a7e2bfdcf7b334a9c1d6c1ec376b9556cb4bf5
Chipili Kafwilo <ckafwilo@gmail.com>
2026-01-11 23:26:21 +0200

    docs: refactor CodePreview component to simplify tab structure

--- packages/docs/src/components/CodePreview.tsx
+++ packages/docs/src/components/CodePreview.tsx
@@ -12,32 +12,30 @@ export default function CodePreview({ children, code }: CodePreviewProps) {
   return (
     <div style={{ marginBottom: '2rem' }}>
       <Tabs
-        children={
-          <>
-            <TabItem
-              value="preview"
-              label="Preview"
-              default
-              children={
-                <div
-                  style={{
-                    border: '1px solid var(--ifm-color-emphasis-200)',
-                    borderRadius: 'var(--ifm-global-radius)',
-                    padding: '2rem',
-                    backgroundColor: 'var(--ifm-background-surface-color)',
-                  }}
-                >
-                  {children}
-                </div>
-              }
-            />
-            <TabItem
-              value="code"
-              label="Code"
-              children={<CodeBlock language="tsx" children={code} />}
-            />
-          </>
-        }
+        children={[
+          <TabItem
+            value="preview"
+            label="Preview"
+            default
+            children={
+              <div
+                style={{
+                  border: '1px solid var(--ifm-color-emphasis-200)',
+                  borderRadius: 'var(--ifm-global-radius)',
+                  padding: '2rem',
+                  backgroundColor: 'var(--ifm-background-surface-color)',
+                }}
+              >
+                {children}
+              </div>
+            }
+          />,
+          <TabItem
+            value="code"
+            label="Code"
+            children={<CodeBlock language="tsx" children={code} />}
+          />,
+        ]}
       />
     </div>
   );
